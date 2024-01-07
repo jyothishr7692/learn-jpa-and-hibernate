f7b7f6a6c65001596fadde20789d3c0fc06179c7
jrnair01 <jrnair01@JYOTHTRVFM02.corp.teranet.ca>
2024-01-07 23:21:48 +0530

    Inserting and Deleting Data using Spring JDBC

--- src/main/java/com/jyo/learnjpaandhibernate/course/jdb/CourseJdbcRepository.java
+++ src/main/java/com/jyo/learnjpaandhibernate/course/jdb/CourseJdbcRepository.java
@@ -4,17 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.jyo.learnjpaandhibernate.course.Course;
+
 @Repository
 public class CourseJdbcRepository {
 	@Autowired
 	private JdbcTemplate springJdbcTemplate;
-	
-	private static String INSERT_QUERY = 
-			"""
-			insert into course(id, name, author) values(1, 'Learn AWS', 'jyothish');
+
+	private static String INSERT_QUERY = """
+			insert into course(id, name, author) values(?, ?, ?);
 			""";
+	private static String DELETE_QUERY = """
+			delete from course where id = ?;
+			""";
+
+	public void insert(Course course) {
+		springJdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
+	}
 	
-	public void insert() {
-		springJdbcTemplate.update(INSERT_QUERY);
+	public void deleteById(long id) {
+		springJdbcTemplate.update(DELETE_QUERY, id);
 	}
 }
